2cfb408e8857ab624a87e2115213e315ddb02547
unusual.computers <unusual.computers@gmail.com>
2017-11-17 15:15:01 +0000

    setup updates

--- code/mopidy/mopidyradiorough/setup.py
+++ code/mopidy/mopidyradiorough/setup.py
@@ -5,18 +5,6 @@ import re
 from setuptools import find_packages, setup
 from setuptools.command.install import install
 
-gstreamer_cmd='sudo apt-get install python-gst-1.0 \
-    gir1.2-gstreamer-1.0 gir1.2-gst-plugins-base-1.0 \
-    gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly \
-    gstreamer1.0-tools'.split()
-
-class InstallGstreamerStuff(install):
-    def run(self):
-        from subprocess import call
-        print 'Installing gstreamer dependencies'
-        call(gstreamer_cmd)
-        install.run(self)
-    
 def get_version(filename):
     with open(filename) as fh:
         metadata = dict(re.findall("__([a-z]+)__ = '([^']+)'", fh.read()))
@@ -60,11 +48,4 @@ setup(
         'Programming Language :: Python :: 2',
         'Topic :: Multimedia :: Sound/Audio :: Players',
     ],
-    data_files=[
-        ('/usr/share/applications',['mopidy_radio_rough/radio_rough.desktop']),
-        ('/usr/share/pixmaps',['mopidy_radio_rough/ucc.gif','mopidy_radio_rough/ucc.png','mopidy_radio_rough/ucc_64.png']),
-    ],
-    cmdclass={
-        'install': InstallGstreamerStuff,
-    },
 )
